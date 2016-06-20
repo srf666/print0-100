c2d358c519ccdf0db3d0aded5388be389231d3bb
苏日甫 <1020343499@qq.com>
2016-06-20 11:32:43 +0800

    输出0-100
    例子中用来进行遍历的元素是一组使用闭区间操作符(...)表示的从0到100的数字。number被赋值为闭区间中的第一个数字(0)，然后循环中的语
    句被执行一次。在本例中，这个循环只包含一个语句，用来输出当前number的值。该语句执行后，number的值被更新为闭区间中的第二个数字(1)，
    之后print方法会再执行一次。整个过程会进行到闭区间结尾为止。
    
    上面的例子中，number是一个每次循环遍历开始时被自动赋值的常量。这种情况下，number在使用前不需要声明，只需要将它包含在循环的声明中，就
    可以对其进行隐式声明，而无需使用let关键字声明。

---
+++ main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  2
+//
+//  Created by 20141105039 on 16/6/20.
+//  Copyright © 2016年 20141105039. All rights reserved.
+//
+
+import Foundation
+for number in 0...100{
+    print("\(number)")
+}
+
